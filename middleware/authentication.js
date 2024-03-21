9a56a6bc4bdbb94c0c279e0b2f9cf1a8c93bcdc2
benedict <aliubenedict@gmail.com>
2024-03-21 21:40:47 +0100

    making progress-t

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     //attach the user to the job route
     req.user = { userID: payload.userID, name: payload.name };
-    next();
+    // next();
   } catch (error) {
     throw new UnauthenticatedError("Authentication Invalid");
   }
