0c4155ca5698b6e16ac61895cc99a0c9c680a1ed
benedict <aliubenedict@gmail.com>
2024-03-25 01:04:26 +0100

    making progress-e

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,18 +1,25 @@
+const { object } = require("joi");
 const CustomAPIError = require("../error/customError");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong. Try again later",
+    message: err.message || "Something went wrong, please try again later",
   };
-  //validation error
-  if (err.name === "ValidatorError") {
-    customError.msg = Object.values(err.errors)
-      .map((error) => error.message)
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((err) => err.message)
       .join(", ");
     customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.code && err.code === 11000) {
+    customError.message = `${err.keyValue.email} has already been used by another user. Please try a different email`;
+    customError.statusCode = 400;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res
+    .status(customError.statusCode)
+    .json({ message: customError.message });
 };
 module.exports = errorHandlerMiddleware;
