dbb6e5b020e9722db4121d3a630679bd3dfd7233
benedict <aliubenedict@gmail.com>
2024-03-25 01:17:44 +0100

    making progress-h

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const registerUser = async (req, res) => {
   const { email, password } = req.body;
-  // if (!email || !password) {
-  //   throw new BadRequestError("Please provide email and password");
-  // }
+
   const user = await User.create({ ...req.body });
 
   res.status(StatusCodes.CREATED).json({ user });
--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -3,21 +3,21 @@ const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || "Something went wrong, please try again later",
+    message: err.message || "Something went wrong. Try again later.",
   };
   if (err.name === "ValidationError") {
     customError.message = Object.values(err.errors)
-      .map((err) => err.message)
+      .map((error) => error.message)
       .join(", ");
-    customError.statusCode = 400;
+    customError.message = 400;
   }
   if (err.code && err.code === 11000) {
-    customError.message = `${err.keyValue.email} has already been used by another user. Please try a different email`;
+    customError.message = `${err.keyValue.email} has already been chosen. Please try another email.`;
     customError.statusCode = 400;
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res
     .status(customError.statusCode)
     .json({ message: customError.message });
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 };
 module.exports = errorHandlerMiddleware;
