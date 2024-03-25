bbd0ba526de335522a3381de72e7c374dc3147c2
benedict <aliubenedict@gmail.com>
2024-03-25 01:18:10 +0100

    making progress-j

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,6 @@ const User = require("../model/userModel");
 const { StatusCodes } = require("http-status-codes");
 
 const registerUser = async (req, res) => {
-  const { email, password } = req.body;
-
   const user = await User.create({ ...req.body });
 
   res.status(StatusCodes.CREATED).json({ user });
